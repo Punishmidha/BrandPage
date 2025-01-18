079cc2a4b348928013b89781e4ed879d556df133
Punishmidha <punishmidha4@gmail.com>
2025-01-19 00:53:23 +0530

    firstCommit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+const HeroSection = () => {
+    return <main className="hero container">
+
+        <div className="hero-content">
+            <h1>Your feet deserve the best</h1>
+            <p>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi blanditiis maxime repellat?</p>
+
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+
+            
+        </div>
+        <div className="hero-image">
+            <img src="images\shoes.jpg" alt="hero-image" />
+        </div>
+
+    </main>
+}
+export default HeroSection;
